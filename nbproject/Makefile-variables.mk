cb1de4d65dc8da4f7750646ee69f494349cb1edd
Elagin Pavel <elagin@lanbilling.ru>
2011-02-18 19:24:31 +0300

    first commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=radiusclient
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/radiusclient
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=radiusclient.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/radiusclient.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=radiusclient
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/radiusclient
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=radiusclient.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/radiusclient.tar
